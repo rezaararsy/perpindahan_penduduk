ae381d7abd147404c38bd52bca351ce016c663ea
DESKTOP-N07UC7F\HP <rezaararsy@gmail.com>
2022-11-11 14:56:40 +0700

    SQL

--- perpindahan.sql
+++ perpindahan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 01 Sep 2022 pada 10.52
+-- Waktu pembuatan: 11 Nov 2022 pada 08.56
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 7.4.28
 
@@ -29,17 +29,35 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `perpindahan` (
   `id_perpindahan` int(11) NOT NULL,
-  `kecamatan` int(11) NOT NULL,
-  `kelurahan` int(11) NOT NULL,
-  `rw` int(11) NOT NULL,
-  `rt` int(11) NOT NULL,
-  `nik` int(11) NOT NULL,
+  `kecamatan` varchar(11) NOT NULL,
+  `kelurahan` varchar(11) NOT NULL,
+  `rw` varchar(11) NOT NULL,
+  `rt` varchar(11) NOT NULL,
+  `nik` varchar(11) NOT NULL,
   `nama` varchar(150) NOT NULL,
-  `jenis_pindah` int(11) NOT NULL,
-  `skpwni` int(11) NOT NULL,
+  `jenis_pindah` varchar(11) NOT NULL,
+  `skpwni` varchar(11) NOT NULL,
   `tgl_pindah` date NOT NULL,
   `alamat_rt` varchar(150) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `perpindahan`
+--
+
+INSERT INTO `perpindahan` (`id_perpindahan`, `kecamatan`, `kelurahan`, `rw`, `rt`, `nik`, `nama`, `jenis_pindah`, `skpwni`, `tgl_pindah`, `alamat_rt`) VALUES
+(7, '1', '1002', '1', '9', '0', 'RISCHKA MOUDY PUSPITASARI', '2', '	SKPWNI/357', '2022-03-30', 'JL TELUK PELABULAN RATU 25 RT 09 RW 01 ARJOSARI BLIMBING'),
+(8, '1', '1002', '4', '7', '0', 'LULUK SHOLIKHATI', '1', '	SKPWNI/350', '2022-03-30', 'JL. TELUK CENDRAWASIH 110 RT 01 RW 02 ARJOSARI BLIMBING'),
+(9, '1', '1002', '1', '1', '0', 'MOKHAMAD MUSLIQ', '2', 'SKPWNI/3573', '2022-03-29', 'JL. TELUK PELABUHAN RATU I / 5-D RT 06 RW 01 ARJOSARI BLIMBING'),
+(15, '1', '1002', '4', '4', '0', 'ARIEF WIDYANTO', '2', 'SKPWNI/3573', '2022-03-22', 'JL. TAMAN RADEN INTAN KAV. 408'),
+(16, '1', '1002', '3', '5', '0', 'BUDI PRIYANTO', '2', 'SKPWNI/3573', '2022-04-12', 'JL. TELUK CENDRAWASIH XIII / 13 A'),
+(17, '1', '1002', '4', '4', '0', 'KAS APRIAL PERDANA', '2', 'SKPWNI/3573', '2022-03-16', 'JL TELUK ETNA III / 6'),
+(18, '1', '1002', '5', '2', '0', 'AMIR MAHMUD', '2', 'SKPWNI/3573', '2022-03-07', 'JL. TELUK CENDRAWASIH 116'),
+(19, '1', '1002', '5', '5', '0', 'ADAM M.H. SABTU', '2', 'SKPWNI/3573', '2022-03-07', 'JL. TELUK KENDARI NO.1'),
+(20, '1', '1002', '5', '6', '0', 'GALUH AFIH NURMALASARI', '2', 'SKPWNI/3573', '2022-03-11', 'JL. TELUK MANADO 8 A'),
+(21, '1', '1002', '3', '3', '0', 'DWIJA WISNU BRATA', '2', 'SKPWNI/3573', '2022-03-21', 'JL. PERUM CITRA ARJOSARI RESIDENCE'),
+(22, '1', '1002', '3', '1', '0', 'LUKAS M. M. WURITIMUR	', '1', 'SKDWNI/3573', '2022-03-30', ' JL. TELUK PELABUHAN RATU NO. 26 B'),
+(27, '1', '1002', '1', '1', '0', 'r', '1', '2', '2022-11-11', 'r');
 
 -- --------------------------------------------------------
 
@@ -86,7 +104,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `perpindahan`
 --
 ALTER TABLE `perpindahan`
-  MODIFY `id_perpindahan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_perpindahan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
