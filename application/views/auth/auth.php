1714a6df1dd29cf6a2682bb0c237c9105a5c2e92
Reza Ararsy <rezaararsy@gmail.com>
2023-01-28 21:14:54 +0700

    Update

--- application/views/auth/auth.php
+++ application/views/auth/auth.php
@@ -9,7 +9,7 @@ $this->load->view('dist/_partials/header1');
         <div class="row">
           <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
             <div class="login-brand">
-              <img src="<?php echo base_url(); ?>assets/logo.png" alt="logo" width="180" style="background-color:white; border-radius:30px" class="shadow-light">
+              <img src="<?php echo base_url(); ?>assets/logo.png" alt="logo" width="180" style="background-color:white; border-radius:30px; padding:20px" class="shadow-light">
               <!-- <h2 style="color: white; margin-top: 20px;">LAPOR PAK !</h1> -->
             </div>
 
