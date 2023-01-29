d8f521a984209dac381c30924d8eb9cb87eeb898
Reza Ararsy <rezaararsy@gmail.com>
2023-01-29 19:07:24 +0700

    Header

--- application/views/dist/_partials/header1.php
+++ application/views/dist/_partials/header1.php
@@ -8,7 +8,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
   <!-- <title><?php echo $title; ?> &mdash; Aplikasi Perpindahan Penduduk</title> -->
   <title>Aplikasi Perpindahan Penduduk</title>
-
+  <link rel="icon" href="<?php echo base_url(); ?>assets/logo.png"  type="image/png">
   <!-- General CSS Files -->
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="<?php echo base_url(); ?>assets/modules/fontawesome/css/all.min.css">
